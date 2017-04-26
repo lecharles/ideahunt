cebe11a7dd0310b1d8c2a802e5877141637e6b02
lecharles <charleslozano@gmail.com>
2017-04-26 14:43:53 -0400

    Create Navbar component

--- src/react-components/app.js
+++ src/react-components/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './HomePage';
+import Navbar from './Navbar';
 
 class App extends React.Component {
   constructor() {
@@ -9,7 +10,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <HomePage/>
+      <section>
+        <Navbar/>
+        <HomePage/>
+      </section>
     );
   }
 }
