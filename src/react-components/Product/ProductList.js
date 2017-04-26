469921ddbe1360ff250b6bd14fe97fbc6cf18061
lecharles <charleslozano@gmail.com>
2017-04-26 14:22:54 -0400

    Refactor Product Item component

--- src/react-components/Product/ProductList.js
+++ src/react-components/Product/ProductList.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import ProductItem from './ProductItem';
 
 class ProductList extends React.Component {
   render() {
     return (
-      return (
-        <ul className="product-list">
-          {
-            this.props.productList.map(function(itemFromList, itemID) {
-              return <ProductItem key={itemID} {...itemFromList}/>
-            })
-          }
-        </ul>
-      );
-    }
+      <ul className="product-list">
+        {
+          this.props.productList.map(function(itemOnList, itemID) {
+            return <ProductItem key={itemID} {...itemOnList}/>
+          })
+        }
+      </ul>
+    );
   }
+}
 
 export default ProductList;
