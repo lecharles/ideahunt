2f397b3133fbfc4db526d9d9dc1f8f7dc385bb6d
lecharles <charleslozano@gmail.com>
2017-04-26 10:13:20 -0400

    Add HomePage

--- src/react-components/app.js
+++ src/react-components/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HomePage from './HomePage';
 
 class App extends React.Component {
   constructor() {
@@ -8,7 +9,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <h2>React Componentes</h2>
+      <HomePage/>
     );
   }
 }
