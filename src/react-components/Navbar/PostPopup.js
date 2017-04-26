f8fcf57af4fc44f3bd06a0b7b394bf35041eeaa4
lecharles <charleslozano@gmail.com>
2017-04-26 15:53:35 -0400

    Create PostPopup Component

---
+++ src/react-components/Navbar/PostPopup.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Popup from './Popup';
+
+class PostPopup extends React.Component {
+  handlePost = () => {
+
+  };
+
+  render() {
+    return (
+      <Popup {...this.props} style="post-popup">
+        <header>Post a new product</header>
+        <section>
+          <table>
+            <tbody>
+              <tr>
+                <td>Name</td>
+                <td><input placeholder="Enter product's name"></td>
+              </tr>
+              <tr>
+                <td>Description</td>
+                <td><input placeholder="Enter product's description"></td>
+              </tr><tr>
+                <td>Link</td>
+                <td><input placeholder="http://www..."></td>
+              </tr><tr>
+                <td>Media</td>
+                <td><input placeholder="Pate a direct link to an image"></td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <footer className="post-footer">
+          <button onClick={this.handlePost} className="post-btn">Hunt it!</button>
+        </footer>
+      </Popup>
+    );
+  }
+}
+
+export default PostPopup;
